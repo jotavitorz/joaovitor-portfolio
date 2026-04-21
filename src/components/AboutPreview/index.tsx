108b99c3f6a2661b67871ec5c5a576c5d308cbb2
jotavitorz <joaovitorxz812@gmail.com>
2026-04-20 23:18:49 -0300

    refactor: reorganiza estrutura do curriculo

--- src/components/AboutPreview/index.tsx
+++ src/components/AboutPreview/index.tsx
@@ -37,7 +37,7 @@ export function AboutPreview(){
                     </p>
 
                     <div className="flex w-full gap-10 text-lg">
-                        <a href="../../../public/docs/JoaoVitor_CV_PTBR.pdf" download="JoaoVitor_CV_PTBR.pdf" type="application/pdf" 
+                        <a href="../../../public/docs/JoaoVitor-curriculo.pdf" download="JoaoVitor-curriculo.pdf" type="application/pdf" 
                             className="bg-orange-600 px-4 py-2 rounded-md flex gap-2 items-center text-neutral-200"
                         >
                             <IoMdDocument className="h-5 w-5" /> 
--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -32,7 +32,7 @@ export function Contact() {
 
                         <a 
                             className="flex items-center gap-2 md:hover:-translate-y-2 duration-300 ease-in-out" 
-                            href="../../../public/docs/JoaoVitor_CV_PTBR.pdf" download="JoaoVitor_CV_PTBR.pdf" type="application/pdf"
+                            href="../../../public/docs/JoaoVitor-curriculo.pdf" download="JoaoVitor-curriculo.pdf" type="application/pdf"
                         >
                             <IoMdDocument className="w-10 h-10 text-gray-400" />
                             <span className="font-medium">Curriculo</span>
@@ -48,8 +48,7 @@ export function Contact() {
                     </div>
 
                 </section>
-            </div>
-            
+            </div>  
             <div className="flex-2 w-full h-full ">
                 <ContactPreview />
             </div>
