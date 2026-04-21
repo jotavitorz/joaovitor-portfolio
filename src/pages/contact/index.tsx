c36a9c19a3fdc29d0e079c8955be804afcf6e800
jotavitorz <joaovitorxz812@gmail.com>
2026-04-20 23:47:27 -0300

    fix: ajusta path do arquivo PDF para padrao da pasta public no Vite

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -32,7 +32,7 @@ export function Contact() {
 
                         <a 
                             className="flex items-center gap-2 md:hover:-translate-y-2 duration-300 ease-in-out" 
-                            href="../../../public/docs/JoaoVitor-curriculo.pdf" download="JoaoVitor-curriculo.pdf" type="application/pdf"
+                            href="/docs/JoaoVitor-curriculo.pdf" download="JoaoVitor-curriculo.pdf" type="application/pdf"
                         >
                             <IoMdDocument className="w-10 h-10 text-gray-400" />
                             <span className="font-medium">Curriculo</span>
